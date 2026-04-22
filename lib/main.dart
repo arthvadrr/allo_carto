6db6098bdc37333b4858e4086436b886406a0644
Bryan <54081465+arthvadrr@users.noreply.github.com>
2026-04-21 17:07:40 -0700

    feat: It makes a db now. yay.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
+// Packages
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+// App
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Spin up the DB if it doesn't exist
+  if (!kIsWeb &&
+      (defaultTargetPlatform == TargetPlatform.macOS ||
+          defaultTargetPlatform == TargetPlatform.windows ||
+          defaultTargetPlatform == TargetPlatform.linux)) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const AlloCartoApp());
 }
