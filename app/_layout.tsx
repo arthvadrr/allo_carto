5e75b3ce6db88b084653b62c82962fdf8d936bba
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2026-05-11 22:49:58 -0500

    feat: working tabs / nav (#12)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+/*
+ * Stack navigator
+ * https://docs.expo.dev/router/advanced/stack/
+ * 
+ * Note: 
+ * Showing the header literally shows "(tabs)" in the header, 
+ * read about the stack component.
+ */
+export default function AppLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
