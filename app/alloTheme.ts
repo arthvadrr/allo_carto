99cfd8100e95b8300aabfbbebc0bd217e1f2fc78
Bryan <54081465+arthvadrr@users.noreply.github.com>
2026-05-13 16:05:56 -0500

    feat: ThemeProvider wrapper and theme file

---
+++ app/alloTheme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme, Theme } from '@react-navigation/native';
+
+const alloTheme: Theme = {
+	dark: false,
+	colors: {
+		primary: 'rgb(10, 132, 255)',
+		background: 'rgb(1, 1, 1)',
+		card: 'rgb(18, 18, 18)',
+		text: 'rgb(229, 229, 231)',
+		border: 'rgb(39, 39, 41)',
+		notification: 'rgb(255, 69, 58)',
+	},
+	fonts: { ...DefaultTheme.fonts },
+};
+
+export default alloTheme;
