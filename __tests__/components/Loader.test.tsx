4b09b66fdf12b54e3ce51f7752dd34fa05edf824
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2026-05-27 17:10:54 -0500

    feat: a few tests written and jest setup (#54)

---
+++ __tests__/components/Loader.test.tsx
@@ -0,0 +1,10 @@
+import Loader from '@/src/components/Loader';
+import { render } from '@testing-library/react-native';
+
+describe('<Loader />', () => {
+  test('The loader is rendering the correct text', () => {
+    const { getByText } = render(<Loader />);
+
+    getByText('Loading...');
+  });
+});
